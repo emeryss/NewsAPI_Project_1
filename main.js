793ba67b48cefd5453d37f1816156083933868c2
XLCOR <emeryss.steam@gmail.com>
2024-02-21 22:38:53 +0900

    feb 21 2nd

--- main.js
+++ main.js
@@ -3,15 +3,15 @@ const API_KEY = `ac0ee77ab7cb4975a4485d0265c3fc85`
 let newsList = []
 
 const getLatestNews = async ()=>{
-    /* 여기는 noona newsAPI (API KEY 필요 없음)
+    // 여기는 noona newsAPI (API KEY 필요 없음)
     let q = ""
     let page = 1
     let pageSize = 20
     let category = "technology"
     const url = new URL(`https://newsapi-1.netlify.app/top-headlines?category=${category}&page=${page}&pageSize=${pageSize}`);
-    // 여기까지 noona news API */
+    // 여기까지 noona news API 
 
-    const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+    //const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
     const response = await fetch(url)
     const data = await response.json()
     newsList = data.articles
