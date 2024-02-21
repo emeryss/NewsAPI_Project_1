907794b000d91c380b53128cf15c1db79b658603
XLCOR <emeryss.steam@gmail.com>
2024-02-22 00:57:13 +0900

    feb 21 몇번째??

--- main.js
+++ main.js
@@ -40,4 +40,20 @@ const render=()=>{
     document.getElementById("news-board").innerHTML=newsHTML
 }
 
+const openNav = () => {
+    document.getElementById("mySidenav").style.width = "250px";
+};
+const closeNav = () => {
+    document.getElementById("mySidenav").style.width = "0";
+};
+
+const openSearchBox = () => {
+    let inputArea = document.getElementById("input-area");
+    if (inputArea.style.display === "inline") {
+      inputArea.style.display = "none";
+    } else {
+      inputArea.style.display = "inline";
+    }
+};
+
 getLatestNews();
