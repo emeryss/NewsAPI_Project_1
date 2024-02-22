43d6949ac2799fe8275e6fd7bf6a4c424be01f76
XLCOR <emeryss.steam@gmail.com>
2024-02-23 00:56:49 +0900

    feb22 1st

--- main.js
+++ main.js
@@ -1,17 +1,12 @@
 const API_KEY = `ac0ee77ab7cb4975a4485d0265c3fc85`
 
 let newsList = []
+const menus = document.querySelectorAll(".menus button")
+menus.forEach(menu=>menu.addEventListener("click", (event)=>getNewsByCategory(event)))
 
 const getLatestNews = async ()=>{
-    // 여기는 noona newsAPI (API KEY 필요 없음)
-    let q = ""
-    let page = 1
-    let pageSize = 20
-    let category = "technology"
-    const url = new URL(`https://newsapi-1.netlify.app/top-headlines?category=${category}&page=${page}&pageSize=${pageSize}`);
-    // 여기까지 noona news API 
-
     //const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+    const url = new URL(`https://newsapi-1.netlify.app/top-headlines?country=kr`);
     const response = await fetch(url)
     const data = await response.json()
     newsList = data.articles
@@ -19,6 +14,28 @@ const getLatestNews = async ()=>{
     console.log("ddd",newsList)
 };
 
+const getNewsByCategory = async (event) => {
+    const category = event.target.textContent.toLowerCase();
+    //const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${API_KEY}`);
+    const url = new URL(`https://newsapi-1.netlify.app/top-headlines?country=kr&category=${category}`);
+    const response = await fetch(url);  
+    const data = await response.json();
+    newsList = data.articles;
+    render();
+}
+
+const getNewsByKeyword = async () => {
+    const keyword = document.getElementById("search-input").value;
+    console.log("keyword", keyword)
+    //const url = new URL(`https://newsapi.org/v2/top-headlines?q=${keyword}&country=us&apiKey=${API_KEY}`)
+    const url = new URL(`https://newsapi-1.netlify.app/top-headlines?q=${keyword}&country=kr`);
+    const response = await fetch(url);  
+    const data = await response.json();
+    console.log("keyword data", data);
+    newsList = data.articles;
+    render();
+}
+ 
 const render=()=>{
     const newsHTML = newsList.map(
         (news)=>`<div class ="row news">
@@ -57,3 +74,7 @@ const openSearchBox = () => {
 };
 
 getLatestNews();
+
+//1. 버튼들에 클릭 이벤트 주기
+//2. 카테고리별 뉴스 가져오기
+//3. 그 뉴스를 보여주기
