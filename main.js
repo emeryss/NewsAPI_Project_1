b4461592c27a0b4d34b635485634408d9745aa6b
XLCOR <emeryss.steam@gmail.com>
2024-02-23 17:44:57 +0900

    feb23 1st take

--- main.js
+++ main.js
@@ -1,44 +1,64 @@
-const API_KEY = `ac0ee77ab7cb4975a4485d0265c3fc85`
+const API_KEY = `ac0ee77ab7cb4975a4485d0265c3fc85`;
 
-let newsList = []
-const menus = document.querySelectorAll(".menus button")
-menus.forEach(menu=>menu.addEventListener("click", (event)=>getNewsByCategory(event)))
+let newsList = [];
+const menus = document.querySelectorAll(".menus button");
+menus.forEach((menu) =>
+    menu.addEventListener("click", (event) => getNewsByCategory(event))
+);
 
-const getLatestNews = async ()=>{
-    //const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
-    const url = new URL(`https://newsapi-1.netlify.app/top-headlines?country=kr`);
-    const response = await fetch(url)
-    const data = await response.json()
-    newsList = data.articles
-    render()
-    console.log("ddd",newsList)
+let url = new URL(
+    `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+);
+// let url = new URL(`https://newsapi-1.netlify.app/top-headlines?country=kr`)
+
+const getNews = async () => {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        if (response.status === 200) {
+                if(data.articles.length ===0){
+                    throw new Error("No results for this search")
+                }
+            newsList = data.articles;
+            render();
+        } else {
+            throw new Error(data.message)
+        }
+    } catch (error) {
+        errorRender(error)
+    }
+};
+
+const getLatestNews = async () => {
+    url = new URL(
+        `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+    );
+    //url = new URL(`https://newsapi-1.netlify.app/top-headlines?country=kr`);
+    getNews();
 };
 
 const getNewsByCategory = async (event) => {
     const category = event.target.textContent.toLowerCase();
-    //const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${API_KEY}`);
-    const url = new URL(`https://newsapi-1.netlify.app/top-headlines?country=kr&category=${category}`);
-    const response = await fetch(url);  
-    const data = await response.json();
-    newsList = data.articles;
-    render();
-}
+    url = new URL(
+        `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${API_KEY}`
+    );
+    //url = new URL(`https://newsapi-1.netlify.app/top-headlines?country=kr&category=${category}`);
+    getNews();
+};
 
 const getNewsByKeyword = async () => {
     const keyword = document.getElementById("search-input").value;
-    console.log("keyword", keyword)
-    //const url = new URL(`https://newsapi.org/v2/top-headlines?q=${keyword}&country=us&apiKey=${API_KEY}`)
-    const url = new URL(`https://newsapi-1.netlify.app/top-headlines?q=${keyword}&country=kr`);
-    const response = await fetch(url);  
-    const data = await response.json();
-    console.log("keyword data", data);
-    newsList = data.articles;
-    render();
-}
- 
-const render=()=>{
-    const newsHTML = newsList.map(
-        (news)=>`<div class ="row news">
+    url = new URL(
+        `https://newsapi.org/v2/top-headlines?country=us&q=${keyword}&apiKey=${API_KEY}`
+    );
+    // url = new URL(`https://newsapi-1.netlify.app/top-headlines?q=${keyword}&country=kr`);
+    getNews();
+};
+
+const render = () => {
+    const newsHTML = newsList
+        .map(
+            (news) => `<div class ="row news">
     <div class="col-lg-4">
         <img class="news-img-size" src=${news.urlToImage}/>
     </div>
@@ -52,10 +72,11 @@ const render=()=>{
         </div>
     </div>
 </div>`
-    ).join('')
+        )
+        .join("");
 
-    document.getElementById("news-board").innerHTML=newsHTML
-}
+    document.getElementById("news-board").innerHTML = newsHTML;
+};
 
 const openNav = () => {
     document.getElementById("mySidenav").style.width = "250px";
@@ -67,14 +88,18 @@ const closeNav = () => {
 const openSearchBox = () => {
     let inputArea = document.getElementById("input-area");
     if (inputArea.style.display === "inline") {
-      inputArea.style.display = "none";
+        inputArea.style.display = "none";
     } else {
-      inputArea.style.display = "inline";
+        inputArea.style.display = "inline";
     }
 };
 
-getLatestNews();
+const errorRender = (error)=>{
+    const errorHTML = `<div class="alert alert-danger" role="alert">
+  ${error.message}
+</div>`
 
-//1. 버튼들에 클릭 이벤트 주기
-//2. 카테고리별 뉴스 가져오기
-//3. 그 뉴스를 보여주기
+document.getElementById("news-board").innerHTML=errorHTML
+}
+
+getLatestNews();
